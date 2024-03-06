3473ce3f28362d823dac279d2f87659260543d6b
Alexander784 <alexandernyaga1999@gmail.com>
2024-03-06 16:04:46 +0300

    Style the cards

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const NewsItem = ({ title, description,src, url }) => {
   return (
-    <div className="card" style={{maxWidth:"345px"}}>
-  <img src={src} className="card-img-top" alt="..."/>
+    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
+  <img src={src} style={{height:"200px",width:"360px"}} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description?description.slice(0,90):"News "}</p>
     <a href={url} className="btn btn-primary">Read More</a>
   </div>
 </div>
